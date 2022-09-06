59be34faaf30c3bbd8533ccfc2d7510b26c439c9
pem2k <107086485+pem2k@users.noreply.github.com>
2022-09-06 14:14:25 -0700

    allowing all origins

--- server.js
+++ server.js
@@ -10,18 +10,7 @@ var corsOptions = {
     credentials: true,
     methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'] };
 
-
-app.use((req, res, next) => {
-    const allowedOrigins = ['http://localhost:3000', 'https://radiant-naiad-1954e2.netlify.app'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-         res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
-    return next();
-  });
+    app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
